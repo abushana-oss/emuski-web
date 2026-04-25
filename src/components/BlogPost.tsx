b0c86a21f9db6951f92861d46bdf625e7dd41cdc
Heena <heenana.sk31@gmail.com>
2026-04-25 13:27:58 +0530

    fix: remove duplicate style prop in BlogPost.tsx

--- src/components/BlogPost.tsx
+++ src/components/BlogPost.tsx
@@ -1091,7 +1091,6 @@ export const BlogPostComponent = ({ post, allPosts }: BlogPostComponentProps) =>
                       : 'lg:sticky lg:top-20 lg:self-start'
                   }`}
                   style={{ maxHeight: 'calc(100vh - 5rem)' }}
-                  style={{ maxHeight: 'calc(100vh - 5rem)' }}
                   onClick={(e) => e.stopPropagation()}
                 >
                   {/* Mobile Close Button */}
